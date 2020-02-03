c51d33f1f8aa35d0d312b4e80f415bf6931d2c87
Jacob Thompson <Jacobdt@msn.com>
2020-02-03 09:44:47 -0700

    feat: 'created' the package, moved initial extract functionality over; todo: determine filetype

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/DusanKasan/parsemail v0.0.0-20190115161936-abc648830b9a
+	github.com/pkg/errors v0.9.1
 	github.com/richardlehane/mscfb v1.0.3
 )
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
-	"strings"
-
-	"github.com/richardlehane/mscfb"
+	"main/DocStract"
 )
 
 func main() {
@@ -16,47 +13,15 @@ func main() {
 		panic(err)
 	}
 
-	reader := bytes.NewReader(data)
-
-	doc, err := mscfb.New(reader)
-
+	files, count, err := DocStract.Extract(data)
 	if err != nil {
 		panic(err)
 	}
 
-	files := [][]byte{}
-	attachment := false
-	file := 0
+	fmt.Println("Found ", count, " files what can we do with these?")
 
-	for entry, err := doc.Next(); err == nil; entry, err = doc.Next() {
-
-		//fmt.Println(entry.Name, entry.Size, entry.Path, entry.Initial)
-
-		if strings.Contains(entry.Name, "attach") {
-			files = append(files, []byte{})
-			attachment = true
-			continue
-		}
-		if attachment && strings.Contains(entry.Name, "properties") {
-			attachment = false
-			file++
-			continue
-		}
-
-		if attachment {
-			buf := make([]byte, entry.Size)
-			i, _ := entry.Read(buf)
-			if i > 0 {
-				files[file] = append(files[file], buf[:i]...)
-			}
-		}
-
-	}
-
-	fmt.Println("Found ", len(files), " files what can we do with these?")
-
-	for a, binaryData := range files {
-		err := ioutil.WriteFile(fmt.Sprintf("%d.pdf", a), binaryData, 0644)
+	for a, binaryData := range *files {
+		err := ioutil.WriteFile(fmt.Sprintf("%d.pdf", a), binaryData.Bytes, 0644)
 		if err != nil {
 			panic(err)
 		}
